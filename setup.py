023ea1f3cc29274036f79a19440241353eff6f34
mthorrell <michael.horrell@uptake.com>
2019-11-24 14:31:15 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="draw_gym-mthorrell", # Replace with your own username
+    version="0.0.1",
+    author="mthorrell",
+    author_email="",
+    description="RL environment for drawing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mthorrell/draw_gym",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
