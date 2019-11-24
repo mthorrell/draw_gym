d7920dca520e79dd4a1e15539bdace71a3529515
mthorrell <michael.horrell@uptake.com>
2019-11-24 14:37:22 -0500

    updated python version

--- draw_gym/__init__.py
+++ draw_gym/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='Drawenv-v0',
-    entry_point='src.draw_env:DrawEnv',
+    entry_point='draw_gym.draw_env:DrawEnv',
     max_episode_steps=10,
     reward_threshold=0.0,
 )
